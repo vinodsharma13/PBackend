e81b4355b709a6e2efd6f1979a31e5ceb207ad95
vinod sharma <vinodsharma.india@hotmail.com>
2025-03-08 20:10:16 +0530

    admim panel updated

--- PBackend/urls.py
+++ PBackend/urls.py
@@ -2,13 +2,19 @@
 from django.contrib import admin
 from django.urls import path,include
 
-from django.conf import settings  # add this
-from django.conf.urls.static import static  # add this
+from django.conf import settings
+from django.conf.urls.static import static
 
+from django.http import HttpResponseRedirect
+
+def redirect_to_admin(request):
+    return HttpResponseRedirect('/admin/')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', redirect_to_admin),  # ✅ Redirect home to admin panel
 ]
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- dictionary/admin.py
+++ dictionary/admin.py
@@ -2,20 +2,27 @@ from django.contrib import admin
 from .models import Paribhasha, ParibhashaLine
 
 
+
 class ParibhashaLineInline(admin.TabularInline):  # This creates a related form
     model = ParibhashaLine
     extra = 1  # Show one blank line by default
     min_num = 1  # Minimum 1 line required
-    max_num = 50  # Maximum 50 lines per Paribhasha
+    max_num = 10  # Maximum 50 lines per Paribhasha
     fields = ['name']  # Only show the name field
 
 
 class ParibhashaAdmin(admin.ModelAdmin):
     list_display = ('hindi', 'hinglish', 'pageno')
     search_fields = ('hindi', 'hinglish')
-    # list_filter = ('pageno',)
+    list_filter = ('pageno',)
     inlines = [ParibhashaLineInline]  # Attach the inline form here
 
 
 # Register the models
 admin.site.register(Paribhasha, ParibhashaAdmin)
+
+
+# ✅ Change the admin panel title and header
+admin.site.site_header = "Paribhasha Administrator"
+admin.site.site_title = "Paribhasha Admin Panel"
+admin.site.index_title = "Welcome to Paribhasha Administrator"
